0c6081705be4eb99240c1b30ac05766dae1a9e3a
MarioMerZap <mariomerzap@uma.es>
2025-02-19 09:06:51 +0100

    Cambios que quitan backend y frontend

--- Parcial_3/backend/src/routes/projectionRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const projectionController = require("../controllers/projectionController");
-
-router.post("/", projectionController.create);
-router.get("/search", projectionController.search);
-
-module.exports = router;
